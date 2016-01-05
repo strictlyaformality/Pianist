899a2525ffa3831e2ea3cacc7e806d5689cc78f4
strictlyaformality <kreell.j@husky.neu.edu>
2016-01-04 21:50:37 -0500

    Refactor the way attributes are stored on the jquery object

--- jquery.pianist.js
+++ jquery.pianist.js
@@ -3,6 +3,9 @@
     options = options || {};
     method = method || 'init';
 
+    //either there are two aguments (method and options) or none (init)
+    if(!(arguments.length === 2 || aguments.length === 0)) { return; }
+
     //start utilities ----------------------------------------|
 
     //stop event propagation / event defaults
@@ -20,18 +23,23 @@
 
     //initialize the pianist object
     var init = function(options){
-      this._keyMap = options.keyMap || {};
-      this._defaultEvent = options.defaultEvent || 'keydown';
-      this._killList = options.killList || [];
+      this._pianist = {
+        keyMap : options.keyMap || {},
+        defaultEvent : options.defaultEvent || 'keydown',
+        killList : options.killList || []
+      }
     };
 
     var handle = function(event){
+      //if pianist hasn't been initialized, don't handle anything
+      if (!this._pianist) { return; }
+
       //get the key code and event type
       var key = event.keyCode || event.which,
           type = event.type;
 
       //get the handler function or object from the keyMap
-      var handler = this._keyMap[key];
+      var handler = this._pianist.keyMap[key];
 
       //case 1: handler is a function...
       //  case 1a: event is the default event type, execute handler
@@ -41,30 +49,34 @@
       //  case 2b: event is not in the object, execute a no-op
       //case 3: handler is neither a function nor object, execute a no-op
       var keyFunction = typeof handler === 'function'
-        ? (type === this._defaultEvent ? handler : noop)
+        ? (type === this._pianist.defaultEvent ? handler : noop)
         : (typeof handler === 'object' ? handler[type] || noop : noop);
 
       //execute the function
       keyFunction.call(this, event);
 
       //if the key is in the kill list, kill the event
-      if (this._killList.indexOf(key) !== -1) killEvent(event);
+      if (this._pianist.killList.indexOf(key) !== -1) killEvent(event);
     };
 
     //handlers: {x0 : f0, ... , xN : fN}
     //  such that: xT = Typeof Number, fT = Typeof Function
     //keyKillList: [Typeof Number]
     var addHandler = function(handlers, killList){
-      $.extend(this._keyMap, keymap || {});
-      $.extend(this._killList, killList || []);
+      if(!this._pianist){ return; }
+
+      $.extend(this._pianist.keyMap, keymap || {});
+      $.extend(this._pianist.killList, killList || []);
     };
 
     //key: Typeof Number
     var removeHandler = function(key){
-      var index = this._killList.indexOf(key);
+      if(!this._pianist){ return; }
 
-      if (this._keyMap[key]) delete this._keyMap[key];
-      if (index !== -1) this._killList.splice(index, 1);
+      var index = this._pianist.killList.indexOf(key);
+
+      if (this._pianist.keyMap[key]) delete this._pianist.keyMap[key];
+      if (index !== -1) this._pianist.killList.splice(index, 1);
     };
     //end exposed methods -----------------------------------|
 
@@ -76,6 +88,7 @@
       removeHandler : removeHandler
     }
 
-    if(method in methods){ return methods[method].call(this, options); }
+    //the method either exists or is undefined. call a noop if undefined.
+    return (methods[method] || noop).call(this, options);
   };
 })(window.jQuery);
